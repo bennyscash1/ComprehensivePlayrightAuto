10ff40dcdddbf57768c80f25f07ec7717f5aa25a
bennyscash1 <127658116+bennyscash1@users.noreply.github.com>
2025-06-14 21:58:39 +0300

    init main

---
+++ WebTest/Flows/AiWebFlow/WebAiFlow.cs
@@ -0,0 +1,24 @@
+﻿using ComprehensiveAutomation.Test.PageObject;
+using ComprehensiveAutomation.Test.UiTest.Tests.Flows;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComprehensivePlayrightAuto.WebTest.Flows.AiWebFlow
+{
+    public class WebAiFlow : BaseFlows
+    {
+        public IPage pDriver;
+        LoginPage loginPage;
+
+        public WebAiFlow(IPage i_driver) : base(i_driver)
+        {
+            pDriver = i_driver;
+            loginPage = new LoginPage(i_driver);
+
+        }
+    }
+}
